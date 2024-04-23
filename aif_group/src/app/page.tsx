89305374fd29ef93a74c925250eedcaf5844ae76
surra7 <subum6302@gmail.com>
2024-04-23 11:48:19 +0900

    :lipstick: style: 헤더 UI 완성
    헤더 컴포넌트 UI 완성, global css, tailwind extend 수정
    
    fixes: #3

--- aif_group/src/app/page.tsx
+++ aif_group/src/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from 'next/image';
+import Header from '@/components/Header';
+import AiImageSection from '@/containers/AiImageSection';
+import AboutSection from '@/containers/AboutSection';
+import MainBanner from '@/containers/MainBanner';
+import HowToCustomSection from '@/containers/HowToCustomSection';
+import LastBanner from '@/containers/LastBanner';
 
 export default function Home() {
-  return <div>홈</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <MainBanner />
+        <AboutSection />
+        <AiImageSection />
+        <HowToCustomSection />
+        <LastBanner />
+      </main>
+      <footer>footer</footer>
+    </>
+  );
 }
--- aif_group/tailwind.config.ts
+++ aif_group/tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './src/**/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
@@ -16,6 +12,27 @@ const config: Config = {
         btn_bg: '#b4b4b4',
         btn_text: '#6c6c6c',
         bg: '#f7f7f7',
+      },
+      height: {
+        header: '142px',
+        banner: '820px',
+        footer: '300px',
+      },
+      animation: {
+        slide1: '64s linear 0s infinite normal forwards running slide1',
+        slide2: '64s linear 0s infinite normal forwards running slide2',
+      },
+      keyframes: {
+        slide1: {
+          '0%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-100%)' },
+          '50.01%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slide2: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-200%)' },
+        },
       },
     },
   },
