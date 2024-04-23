57e1bc0667375c22148baea8d0e6ef09679e7785
surra7 <subum6302@gmail.com>
2024-04-23 23:02:33 +0900

    :lipstick: design: 헤더, about, 메인 섹션 UI 수정
    헤더, about 섹션, 메인 배너 UI 수정
    
    Fixes: #3

--- aif_group/src/components/Header.tsx
+++ aif_group/src/components/Header.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className="w-full h-header px-[20.625rem] py-[37px] flex justify-center items-center">
+    <div className="w-full h-header px-[3rem] py-[2.3125rem] flex justify-center items-center">
       <div className="w-[78.75rem] flex flex-row justify-between items-center">
         <button>
           <Image alt="logo" src={'/icons/logo.svg'} width={237} height={68} />
         </button>
-        <div className="w-[49.8125rem] flex justify-between ml-20">
-          <button className="text-[1.375rem] text-[#3A3A3A] font-medium">about</button>
+        <div className="w-[49.8125rem] flex justify-between ml-12">
+          <button className="text-[1.375rem] text-[#3A3A3A] font-medium">About</button>
           <button className="text-[1.375rem] text-[#3A3A3A] font-medium">생성 이미지</button>
-          <button className="text-[1.375rem] text-[#3A3A3A] font-medium">사용 방법</button>
+          <button className="text-[1.375rem] text-[#3A3A3A] font-medium">사용방법</button>
           <button className="text-[1.375rem] w-[12.5rem] h-[3.25rem] bg-black text-main_active group hover:bg-main_active active:bg-main_active rounded-md font-bold">
             <div className="group-hover:text-black group-active:text-black">디자인 시작하기</div>
           </button>
--- aif_group/src/containers/MainBanner.tsx
+++ aif_group/src/containers/MainBanner.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function MainBanner() {
   return (
     <main className="w-full h-banner bg-[url('/images/main_banner.svg')] bg-cover relative bg-center">
-      <div className="absolute top-2/4 left-1/3 flex flex-col items-end transform -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute top-2/4 left-[36%] flex flex-col items-end transform -translate-x-1/2 -translate-y-1/2">
         <Image alt="main_banner_logo" src={'/icons/main_banner_logo.svg'} width={451} height={56} />
         <div className=" mt-[1.125rem] mr-[4px]">
           <DesignStartButton />
