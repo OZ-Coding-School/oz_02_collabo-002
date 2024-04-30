d37c5b57d7a3ca2c8924f05a5e41e2d1136b2c27
surra7 <subum6302@gmail.com>
2024-04-30 23:31:13 +0900

    :bulb: chore: 디자인 페이지 page.tsx 수정
    디자인 페이지 page.tsx 컴포넌트들 머지를 위해  삭제
    
    Resolves: #19

--- aif_group/src/app/design/page.tsx
+++ aif_group/src/app/design/page.tsx
@@ -1,20 +1,11 @@
 import DesignHeader from '@/containers/design/DesignHeader';
-import DesignLoadingBox from '@/containers/design/DesignLoadingBox';
-import DesignPreviewBox from '@/containers/design/DesignPreviewBox';
-import DesignSelectBox from '@/containers/design/DesignSelectBox';
-import DesignStartBox from '@/containers/design/DesignStartBox';
 
 export default function Design() {
   return (
     <main className="h-screen">
       <DesignHeader />
       <section className="bg-bg w-full h-[calc(100vh-8.875rem)] flex justify-center items-center">
-        <div className="w-full flex flex-row justify-around items-center">
-          {/* <DesignStartBox /> */}
-          {/* <DesignLoadingBox /> */}
-          {/* <DesignSelectBox /> */}
-          <DesignPreviewBox />
-        </div>
+        <div className="w-full flex flex-row justify-around items-center"></div>
       </section>
     </main>
   );
