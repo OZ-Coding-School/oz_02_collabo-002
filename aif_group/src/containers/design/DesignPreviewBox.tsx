a4b29fe3209883e7fbd4911384bf7129451f7dd3
surra7 <subum6302@gmail.com>
2024-05-03 10:20:01 +0900

    :bulb: chore: 이미지 다운로드 화질 개선을 위해 캔버스 코드 수정
    이미지 다운로드 화질 개선을 위헤 캔버스 크기 키우고 캔버스 랜더링안하도록 코드 수정
    
    Resolves: #34

--- aif_group/src/containers/design/DesignPreviewBox.tsx
+++ aif_group/src/containers/design/DesignPreviewBox.tsx
@@ -6,45 +6,51 @@ import downloadImage from '@/utils/downloadImages';
 
 const DesignPreviewBox = () => {
   const [selectedColor, setSelectedColor] = useState('white');
-  const canvasRef: RefObject<HTMLCanvasElement> = useRef<HTMLCanvasElement>(null);
   const [imageDataUrl, setImageDataUrl] = useState('');
-  let imageFile: { imageUrl: string; imageName: string }[] = [];
+  const [imageFile, setImageFile] = useState<{ imageUrl: string; imageName: string }[]>([]);
   const router = useRouter();
 
+  const tshirtImage = {
+    white: '/images/t-shirt_white.png',
+    black: '/images/t-shirt_black.png',
+  };
+
   const imagePath = {
-    whiteTShirt: '/images/t-shirt_white.png',
-    blackTShirt: '/images/t-shirt_black.png',
     aiImage1:
       'https://lh3.googleusercontent.com/fife/ALs6j_Hp2PEtE1a1A02tz07GeGJMclDB2N7fSB4yqYMaJKmoTgnUD8P21ir7opBoG2SU-ZOwC_nOj_76gGsBNuDYjOEWl-2mxF-Tmf7ps_hN-9MgNiJhBwuVgZMlHDmt3cC1ETL93j-LllzxijJNtKhyldVK6Raa2cu0LIckQNSy3OwYs90cTSv0u-KqOpQeDd6E9opHwhBwUSBEpqD8Uo-bmS1eh68W3L0ZjGrJQ8n3ZZdEHDSXErphhvfS-ZbP_ER-ayW35YGqlnZu1RVUflyLoaSW6BAagdPppAl9Fn1u2AsHc2qCIRFd6scYEBYrhUzxNnCxgrYnBMQTTvBG9ADkm9P3XX_LrYHeQDBhrrT3NT_wAUX2C68aq4NIF8QAPrwqWzfkAq3jM6Im9RBjysfjaHlJ-Q08NnbdTR6SbaB3JuLKECgi5eXxodSyQ-tBFI-MV68ygxCG4YRIaM8sFZYkxO0ISvv_fofJWgBpYMxaNLMnsTv1lBbpB_fn1n_33KNHlS1y6lbtrXDPbJoRFS70_B_ziQs_uJuCYyv4F1pHzEFjzxBW5vcOAff0uTLfKwFLTwmemKwjiDxUVtKC8cU5hS8Q5Ktr6Wbei03O83GOzmG9laUVj5Ox6Fr3X_3pKIxurdIw_pdwvrqWJ7AiteaLCo9yrMo-era5pq1RQo5wN7cXTMrsMT9zDMWGh6t-mEPZPX4RTu7iAceZARE0o9cMQonyN4orrgx5H0V7Tzwx3Lfu6wTkMBq1s0Oz7z2mhUOLMhFWhQSM-XY3AFUfehY7fqTz6w5ncGww1ri5C3nsm49xMNQ3WO3XJhkqlquG12J8kubCn7sFrCXkgBuFcQl1nOHVRsvjE049U6mi_NNVXU74TBNvzRvnvEAVUm_MN9AofTR1i81GmomDr33IZ3mkr1YsIQ40qVy3JyRIIYyVXQfGCLXtyLs5LI5RvDdhYe7Ue5JouszaK7EOcVoKm4kXh_CA2vJZ7rjpGrYXlckYq0czkFIs8avIRA8lKQR2_bA6o10pWi37Sm2UsSavjs6_WP8np_2SeEdZ1fEkh7S2sbtuQRjhyDegZ1A9xeTG6wxsBB1y77VS2KkXvv7Vbkx6RC6pOGJYDU5xuhl52J_ZOclK1XkRAsOMTh-1P5OcTH3VB7fM2HleFLMpAgTNjrie3qHHmNm30oUOyYE1nSToiR6ack_J-sCS0fxWV7Zicmy_FciqVz3CBoP6uR9MvpWpCZuduILMODtrTube2tdcAj5EdRdgPSjbXEYx9wye8plF6M-NZTxYU3R1rSK9x-CcBmEephypyISFZ__wbvytoegBIPmx56Y7L4UkH0_6MGMgO3VFIeIfFVX0FW1UqxnR34UCSr2lw75Ve2m6BnGdvA5B-7-80ngh6-3TfrysyxgAlTLCMkoRDl52BNQ7rfil-uTWcv2u1vsBO0JmOchnY2ZyYvS8tCCTbuQxLsuwImDf_TebgIpEJIkqW-jOJM9xj5e43-mXvwigszwP_b6sHF-aNp7_j4LSCNcdGjhsa_pyDzC9PIidDpT-7oHlab_Z0r1LE788zgqsvAkZS-R-RIlux-JP60Cb-17VFxArTIuH5o-dKSFb3dv6MQQiB8fovIgYIRd8tGBpEfi7_oc2Vy0nUGmy8kY=w1774-h1316',
     aiImage2: 'images/aiImage2.png',
+    aiImage3: 'images/aiImage3.png',
   };
 
   useEffect(() => {
-    if (canvasRef?.current) {
-      const canvas = canvasRef.current;
-      const context = canvas?.getContext('2d');
-      context?.clearRect(0, 0, canvas.width, canvas.height);
+    const canvas = document.createElement('canvas');
+    canvas.width = 1800;
+    canvas.height = 1800;
+    const context = canvas.getContext('2d');
+    context?.clearRect(0, 0, canvas.width, canvas.height);
 
-      const Tshirt = new Image();
-      Tshirt.crossOrigin = 'anonymous';
-      Tshirt.src = selectedColor === 'white' ? imagePath.whiteTShirt : imagePath.blackTShirt;
-      Tshirt.onload = () => {
-        context?.drawImage(Tshirt, 0, 0, canvas.width, canvas.height);
+    const Tshirt = new Image();
+    Tshirt.crossOrigin = 'anonymous';
+    Tshirt.src = selectedColor === 'white' ? tshirtImage.white : tshirtImage.black;
+    Tshirt.onload = () => {
+      context?.drawImage(Tshirt, 0, 0, canvas.width, canvas.height);
 
-        const image = new Image();
-        image.crossOrigin = 'anonymous';
-        image.src = imagePath.aiImage1;
-        image.onload = () => {
-          context?.drawImage(image, canvas.width / 2 - 126 / 2, canvas.height / 2 - 126 / 2, 126, 126);
+      const image = new Image();
+      image.crossOrigin = 'anonymous';
+      image.src = imagePath.aiImage1;
+      image.onload = () => {
+        context?.drawImage(image, canvas.width / 2 - 520 / 2, canvas.height / 2 - 520 / 2, 520, 520);
 
-          setImageDataUrl(canvas.toDataURL(`image/png`));
-        };
+        const dataUrl = canvas.toDataURL(`image/png`);
+        setImageDataUrl(dataUrl);
+        setImageFile(prev => [
+          ...prev,
+          { imageUrl: dataUrl, imageName: '티셔츠합성이미지1' },
+          { imageUrl: imagePath.aiImage1, imageName: 'ai생성이미지1' },
+        ]);
       };
-      imageFile.push({ imageUrl: imageDataUrl, imageName: 'ai생성이미지1' });
-      imageFile.push({ imageUrl: imageDataUrl, imageName: 'ai생성이미지2' });
-      console.log(imageFile);
-    }
-  }, [canvasRef, selectedColor, imageFile]);
+    };
+  }, [selectedColor]);
 
   return (
     <div className="w-[27rem] h-[46.875rem] border-[2px] border-black rounded-[16px] shadow-xl">
@@ -60,7 +66,7 @@ const DesignPreviewBox = () => {
         </div>
       </div>
       {/* 티셔츠 색상 버튼 */}
-      <div className="grid grid-cols-2 w-[5.15rem] ml-80">
+      <div className="grid grid-cols-2 w-[5.15rem] ml-80 mb-2">
         <div className="flex flex-col items-center">
           <button
             className={`w-8 h-8 rounded-full border border-text overflow-hidden focus:outline-none ${
@@ -79,9 +85,11 @@ const DesignPreviewBox = () => {
         </div>
       </div>
       <section className="relative flex justify-center items-center">
-        <NextImage src="/icons/arrow_left.svg" alt="T-shirt" width={50} height={50} priority />
-        <canvas width={360} height={380} ref={canvasRef}></canvas>
-        <NextImage src="/icons/arrow_right.svg" alt="T-shirt" width={50} height={50} priority />
+        <NextImage src="/icons/arrow_left.svg" alt="left_arrow" width={50} height={50} priority />
+        <div className="w-[360px] h-[340px] relative">
+          {imageDataUrl && <NextImage src={imageDataUrl} alt="T-shirt" priority fill />}
+        </div>
+        <NextImage src="/icons/arrow_right.svg" alt="right_arrow" width={50} height={50} priority />
       </section>
       <div className="w-[15rem] h-[2.5rem] mt-[3.1375rem] mx-[6.1875rem] flex justify-between">
         <button
