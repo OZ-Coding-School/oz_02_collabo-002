8cd85ee3d986f2edfb61d81d407ae34ac8905863
enjoylonelines <hobeom5022@naver.com>
2024-05-07 02:36:36 +0900

    ⚡️ imageApi type 추가

--- aif_group/src/services/getImages.tsx
+++ aif_group/src/services/getImages.tsx
@@ -1,4 +1,10 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+interface ImageCreationRequest {
+  keyword: string;
+  style: string;
+  [key: string]: string; // 인덱스 시그니처 추가
+}
 
 const apiClient = axios.create({
   baseURL: '/api',
@@ -6,21 +12,21 @@ const apiClient = axios.create({
 });
 
 // 데이터를 URL 인코딩된 형식으로 변환하는 함수
-function encodeFormData(data) {
+function encodeFormData(data: ImageCreationRequest) {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
 }
 
 // POST 요청 보내기
-export async function createImages(keyword: string, style: string) {
+export async function createImages(keyword: string, style: string): Promise<void> {
   try {
     const email = 'b4ur2old@naver.com';
     const data = {
       keyword,
-      style, // 오타 수정
+      style,
     };
-
+    console.log(encodeFormData(data));
     const response = await apiClient.post('/image/create', encodeFormData(data), {
       headers: {
         Cookie: `email=${email}`,
@@ -29,7 +35,16 @@ export async function createImages(keyword: string, style: string) {
 
     console.log('Response:', response.data);
   } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
+    const axiosError = error as AxiosError;
+    if (axiosError.response) {
+      console.error('Error Response:', axiosError.response.data);
+      console.error('Status:', axiosError.response.status);
+      console.error('Headers:', axiosError.response.headers);
+    } else if (axiosError.request) {
+      console.error('Error Request:', axiosError.request);
+    } else {
+      console.error('Error Message:', axiosError.message);
+    }
   }
 }
 
@@ -45,6 +60,15 @@ export async function getImages() {
     console.log('Response:', response.data);
     return response.data;
   } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
+    const axiosError = error as AxiosError;
+    if (axiosError.response) {
+      console.error('Error Response:', axiosError.response.data);
+      console.error('Status:', axiosError.response.status);
+      console.error('Headers:', axiosError.response.headers);
+    } else if (axiosError.request) {
+      console.error('Error Request:', axiosError.request);
+    } else {
+      console.error('Error Message:', axiosError.message);
+    }
   }
 }
