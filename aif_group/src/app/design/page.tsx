31bc42fcf2c2e2d6a6ffb17ccc4c16be669715b1
surra7 <subum6302@gmail.com>
2024-05-07 22:40:19 +0900

    :bulb: chore: 메인페이지 css 수정
    메인배너 이미지 컴포넌트로 수정, 윈도우에 맞춰서 css 수정
    
    Fixes: #44

--- aif_group/src/app/design/page.tsx
+++ aif_group/src/app/design/page.tsx
@@ -13,14 +13,14 @@ export default function Design() {
   return (
     <main className="w-full h-full bg-bg">
       <DesignHeader />
-      <section className="bg-bg w-full h-[calc(100%-8.875rem)] flex justify-center items-center m-auto">
+      <section className="bg-bg w-fit h-[calc(100%-8.875rem)] flex justify-center items-center m-auto">
         {show.alert && (
           <div className="h-full absolute inset-0 bg-black bg-opacity-50 z-10 flex flex-row justify-center items-center">
             <InputAlert onClose={handleStartDesign} />
           </div>
         )}
-        <ul className="w-fit h-full m-auto px-5 flex justify-center items-center flex-1 list-none section">
-          <li className="">
+        <ul className="w-fit h-full m-auto px-5 flex justify-center items-center flex-1 list-none">
+          <li>
             <DesignStartBox onCreateDesign={handleCreateDesign} />
           </li>
           <li className={`ml-5 select-box ${show.selectBox ? 'box-display' : ''}`}>
