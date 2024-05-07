bb62d3f6cc03f614da515394edfd12ae496b95c7
enjoylonelines <hobeom5022@naver.com>
2024-05-07 16:55:26 +0900

    🩹 z-index 수정

--- aif_group/src/app/design/page.tsx
+++ aif_group/src/app/design/page.tsx
@@ -8,9 +8,9 @@ import ErrorAlert1 from '@/containers/modal/ErrorAlert1';
 import ErrorAlert2 from '@/containers/modal/ErrorAlert2';
 import InputAlert from '@/containers/modal/InputAlert';
 import useShowBox from '@/hooks/useShowBox';
+import { useState } from 'react';
 
 export default function Design() {
-
   const [showErrorAlert1, setShowErrorAlert1] = useState(false);
   const [showErrorAlert2, setShowErrorAlert2] = useState(false);
 
@@ -24,11 +24,10 @@ export default function Design() {
     <main className="h-screen">
       <DesignHeader />
       <section
-
         className={`bg-bg w-full h-[calc(100vh-8.875rem)] flex justify-center items-center ${show.alert && 'relative'}
         `}>
         {show.alert && (
-          <div className="absolute inset-0 bg-black bg-opacity-50 z-10 flex justify-center items-center">
+          <div className="absolute inset-0 bg-black bg-opacity-50 z-20 flex justify-center items-center">
             <InputAlert onClose={handleStartDesign} />
           </div>
         )}
@@ -39,16 +38,21 @@ export default function Design() {
         )}
 
         <div className="flex space-x-7 relative z-10 justify-center">
-           {showErrorAlert1 && (
+          {showErrorAlert1 && (
             <div className="absolute top-0 mt-5 transform -translate-y-full z-30">
               <ErrorAlert1 onClose={() => setShowErrorAlert1(false)} show={showErrorAlert1} />
             </div>
           )}
-          <DesignStartBox onCreateDesign={handleCreateDesign}  onError={() => setShowErrorAlert1(true)}  disabled={showErrorAlert1} />
+          <DesignStartBox
+            onCreateDesign={handleCreateDesign}
+            onError={() => setShowErrorAlert1(true)}
+            disabled={showErrorAlert1}
+          />
           <div
             className={`transition-opacity duration-1000 ease-in-out ${show.selectBox ? 'opacity-100' : 'opacity-0'}`}>
-            {!isLoading.create && show.selectBox && <DesignSelectBox onSelectDesign={handleDesignSelection} onRetry={handleRetryDesign} />}
-
+            {!isLoading.create && show.selectBox && (
+              <DesignSelectBox onSelectDesign={handleDesignSelection} onRetry={handleRetryDesign} />
+            )}
           </div>
           {isLoading.create && <DesignLoadingBox type={'select'} />}
           {isLoading.select && <DesignLoadingBox type={'preview'} />}
