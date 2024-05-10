31c854f040f85cb9d4c6eae6f9cc035afaa6dae5
surra7 <subum6302@gmail.com>
2024-05-10 13:40:15 +0900

    :bulb: chore: 마지막 배너 css 수정
    Fixes: #48

--- aif_group/src/containers/main/LastBanner.tsx
+++ aif_group/src/containers/main/LastBanner.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 export default function LastBanner() {
   return (
     <main className="w-full h-banner flex justify-center items-center bg-gradient-to-r from-[#566081] to-[#1F1D22]">
-      <section className="w-full h-[43.75rem] flex flex-col justify-center items-center sm:w-[90%] sm:h-fit">
-        <div className="font-bold text-white text-[2.625rem] text-center mb-[6px] sm:text-[2.125rem]">
+      <section className="w-full h-[43.75rem] flex flex-col justify-center items-center">
+        <div className="font-bold text-white text-[2.625rem] text-center mb-[6px]">
           어느 누구든 <span className="text-main_active">디자이너</span>가 되는 공간 <br />
           <span className="text-main_active">진정한 커스텀</span>을 <span className="text-main_active">무료</span>로
           경험하세요.
@@ -15,7 +15,7 @@ export default function LastBanner() {
           디자인을 몰라도 누구나 손쉽게 자신만의 그래픽 이미지를 만들 수 있습니다.
         </div>
         <DesignStartButton />
-        <div className="sm:w-[80%] h-fit">
+        <div className="h-fit">
           <Image
             alt="last_banner_img"
             src={'/images/last_banner_img.svg'}
