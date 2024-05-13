70dd2c747c3f512c8ce44491a0b4e46224d3999f
enjoylonelines <hobeom5022@naver.com>
2024-05-13 18:29:38 +0900

    🩹 임시 수정

--- aif_group/src/hooks/useValidation.ts
+++ aif_group/src/hooks/useValidation.ts
@@ -1,20 +1,20 @@
 import { postValidation } from '@/services/postValidation';
 import { useQuery } from '@tanstack/react-query';
 
-function useValidation() {
-  const { isLoading, error, refetch } = useQuery({
-    queryKey: ['postValidation', email],
-    queryFn: async () => {
-      try {
-        const res = await postValidation(email);
-        console.log(res);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    enabled: false,
-  });
-  return { isLoading, error, refetch };
-}
+// function useValidation() {
+//   const { isLoading, error, refetch } = useQuery({
+//     queryKey: ['postValidation', email],
+//     queryFn: async () => {
+//       try {
+//         const res = await postValidation(email);
+//         console.log(res);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     },
+//     enabled: false,
+//   });
+//   return { isLoading, error, refetch };
+// }
 
-export default useValidation;
+// export default useValidation;
