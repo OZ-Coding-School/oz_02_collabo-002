eb7da95e049dc140eb2f60d9bb5a534d20d98dba
surra7 <subum6302@gmail.com>
2024-05-22 17:41:01 +0900

    :bug: fix: 토큰 에러 수정, designCount에러 수정

--- aif_group/src/hooks/useShowBox.ts
+++ aif_group/src/hooks/useShowBox.ts
@@ -58,16 +58,6 @@ function useShowBox() {
         setIsLoading(state => ({ ...state, create: false }));
         return;
       }
-
-      setShow(state => ({
-        ...state,
-        startBox: false,
-        selectBox: true,
-        previewBox: false,
-      }));
-      step.current = 2;
-      setDisable(true);
-      setDesignCreateCount(prev => prev + 1);
     } else {
       setShow(state => ({ ...state, errorAlert3: true }));
     }
--- aif_group/src/services/setToken.ts
+++ aif_group/src/services/setToken.ts
@@ -19,6 +19,8 @@ export async function setToken(email: string, status: string, password: string =
 
     if (response.status === 200) {
       //set cookie
+      Cookies.remove('access_token');
+      Cookies.remove('refresh_token');
       const access_token = response.data.access_token;
       const refresh_token = response.data.refresh_token;
       Cookies.set('access_token', access_token, { expires: 1 }); // 1일 후 만료
